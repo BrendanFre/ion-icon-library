4bb76f88bfeb4d2fc8944c1a71b9216e488eb2b0
Brendan Freeman <bfreeman0@gmail.com>
2024-02-22 22:24:58 +0000

    Fixed issue where only one icon would appear

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,33 +1,30 @@
+import { expect, test } from 'vitest';
 import main from '../lib/main';
-import jsImage from '../javascript.svg'
-
-import { expect, test } from 'vitest'
+import jsImage from '../javascript.svg';
 
 test('One element has ionJs will return true', () => {
   const example = document.createElement('div');
   const image = document.createElement('img');
-  console.log(document);
   const app = document.querySelector('body');
-  app.appendChild(example)
+  app.appendChild(example);
 
-  image.src= jsImage
+  image.src = jsImage;
   example.classList.add('ionMd');
   example.classList.add('ionJs');
-  expect(main()).toBe('jsIcon')
-})
+  expect(main()).toBe('jsIcon');
+});
 
 test('No elements has ionJs will return false', () => {
   const example = document.createElement('div');
   const image = document.createElement('img');
-  console.log(document);
   const app = document.querySelector('body');
-  app.appendChild(example)
+  app.appendChild(example);
 
-  image.src= jsImage
+  image.src = jsImage;
   example.classList.add('ionMd');
   example.classList.add('ionJd');
-  expect(main()).toBe('no jsIcon')
-})
+  expect(main()).toBe('no jsIcon');
+});
 
 test('No elements has ionJs will return false', () => {
   const example = document.createElement('div');
@@ -35,13 +32,12 @@ test('No elements has ionJs will return false', () => {
   const example3 = document.createElement('div');
 
   const app = document.querySelector('body');
-  app.appendChild(example)
-  app.appendChild(example2)
-  app.appendChild(example3)
+  app.appendChild(example);
+  app.appendChild(example2);
+  app.appendChild(example3);
   example.classList.add('ionMd');
   example.classList.add('ionJs');
   example2.classList.add('ionJs');
   example3.classList.add('ionJs');
-  expect(main()).toBe('jsIcon')
-})
-
+  expect(main()).toBe('jsIcon');
+});
